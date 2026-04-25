c5c6bbf3ad94be03838e3abd2da0cf0a7def7df9
JustJam <exayntszhenya@gmail.com>
2026-04-25 19:10:55 +0300

    Lab10. General task

---
+++ MySite/news/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('test/', views.test),
+    path('category/<int:category_id>/', views.get_category, name='category'),
+    path('news/<int:news_id>/', views.view_news, name='view_news'),
+]
